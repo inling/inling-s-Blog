db0b3e03dfba5a0b2b465cdb71d7998d4e2a0bee
inling <giraffe.aup@gmail.com>
2018-11-29 16:49:21 +0800

    add base style

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-body{
-  font-family: 微软雅黑, 宋体, Arial, sans-serif;
-  background: rgb(211, 214, 216);
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+/*导入字体*/
+  @font-face {
+      font-family:'Poiret One';
+      src:url('./common/font/PoiretOne-Regular.ttf');
   }
-}
+  @font-face {
+      font-family:'Matchbook';
+      src:url('./common/font/Matchbook.ttf');
+  }
+
+/*公共样式*/
+  /*字体*/
+  $my_ttf1:微软雅黑, 宋体, Arial, sans-serif;
+  $my_ttf2:'Poiret One',Arial,sans-serif;
+  $my_ttf3:'Matchbook',Arial,sans-serif;
+  /*颜色*/
+  $my_gray1:rgb(211, 214, 216);
+  $my_gray2:#b9b9b9;
+  $my_gray3:#9fa3a4;
+  $my_gray4:#818487;
+  $my_violet:#F231BE;
+  $my_blue:#24DEFF;
+  $my_green:#27D338;
+  $my_yellow:#ffcc00;
+
+  body{
+    font-family: $my_ttf1;
+    background: $my_gray1;
+  }
 </style>
