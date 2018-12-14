11b11fe04b04cdc4775b0e4ffaff9174adbc8423
inling <giraffe.aup@gmail.com>
2018-12-14 17:30:24 +0800

    edit file name

---
+++ src/views/InHome.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <InHeader></InHeader>
+    <InSection></InSection>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  components: {
+    InHeader:()=>import('@/components/InHeader.vue'),
+    InSection:()=>import('@/components/InSection.vue')
+  }
+}
+</script>
