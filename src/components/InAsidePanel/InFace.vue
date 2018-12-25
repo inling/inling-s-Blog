6177a767ae298937e7febb15421546b19eac879a
inling <giraffe.aup@gmail.com>
2018-12-25 15:58:48 +0800

    add circle bg

--- src/components/InAsidePanel/InFace.vue
+++ src/components/InAsidePanel/InFace.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="InFace">
-    
+    <div class="circleBox">
+      <div class="circleWrapper">
+        <div class="circleBg">
+          <div class="circleBg">
+            <div class="circleBg">
+              <div class="circleBg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
